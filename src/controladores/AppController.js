668c44be17e00d7a88cf3baae1a59d92b67a2bf9
dorismeza <dorismeza56@gmail.com>
2023-02-20 16:46:31 -0600

    diseño responsive de login y wellcome

--- src/controladores/AppController.js
+++ src/controladores/AppController.js
@@ -1,9 +1,15 @@
 // creamos objeto vacio
 const AppController = {};
 
-
 AppController.index = (req,res) =>{
     res.render('home');
 }
 
+AppController.wellcome = (req,res) =>{
+    res.render('wellcome');
+}
+
+AppController.login = (req,res) =>{
+    res.render('login');
+}
 module.exports = AppController;
--- src/rutas/index.js
+++ src/rutas/index.js
@@ -5,6 +5,7 @@ const AppControler = require('../controladores/AppController');
 //req: lo que recibe esta ruta
 //res: lo que enviamos
 router.get('/',AppControler.index);
-
+router.get('/wellcome',AppControler.wellcome);
+router.get('/login',AppControler.login);
 module.exports = router;
 
